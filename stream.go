53b11b3e824922c1803dbfe81067ce20949843d4
nyarvex <nyarvex@proton.me>
2026-08-31 16:58:52 +0800

    更新 stream.go

--- stream.go
+++ stream.go
@@ -1,222 +1,263 @@
 package mux
-
 import (
-	"io"
-	"net"
-	"os"
-	"sync"
-	"time"
+"errors"
+"io"
+"net"
+"os"
+"sync"
+"sync/atomic"
+"time"
 )
-
+var _ io.ReadWriteCloser = (*Stream)(nil)
 type Stream struct {
-	id            uint32
-	sess          *Session
-	closeCh       chan struct{}
-	closeOnce     sync.Once
-	closeErr      error
-	handshakeOnce sync.Once
-	readDeadline  PipeDeadline
-	writeDeadline PipeDeadline
-	bufMu         sync.Mutex
-	buf           []byte
-	wakeCh        chan struct{}
-	maxBufSize    int
+id                uint32
+sess              *Session
+closeCh           chan struct{}
+closeOnce         sync.Once
+closeErr          error
+readMu            sync.Mutex
+readBuf           []byte
+readCond          *sync.Cond
+writeMu           sync.Mutex
+handshakeOnce     sync.Once
+handshakeCh       chan struct{}
+handshakeChanOnce sync.Once
+handshakeErr      error
+handshakeErrMu    sync.RWMutex
+readDeadline      *PipeDeadline
+writeDeadline     *PipeDeadline
+maxBufSize        int
+timerMu           sync.Mutex
+handshakeTimer    *time.Timer
+handshakeDone     uint32
 }
-
 func newStream(id uint32, sess *Session) *Stream {
-	s := &Stream{
-		id:         id,
-		sess:       sess,
-		closeCh:    make(chan struct{}),
-		wakeCh:     make(chan struct{}, 1),
-		maxBufSize: sess.config.StreamBufferSize,
-	}
-	s.readDeadline = MakePipeDeadline()
-	s.writeDeadline = MakePipeDeadline()
-	return s
+s := &Stream{
+id:            id,
+sess:          sess,
+closeCh:       make(chan struct{}),
+handshakeCh:   make(chan struct{}),
+maxBufSize:    sess.config.StreamBufferSize,
+readDeadline:  NewPipeDeadline(),
+writeDeadline: NewPipeDeadline(),
 }
-
-func (s *Stream) Read(b []byte) (n int, err error) {
-	if len(b) == 0 {
-		return 0, nil
-	}
-	s.bufMu.Lock()
-	defer s.bufMu.Unlock()
-	for {
-		if len(s.buf) > 0 {
-			n = copy(b, s.buf)
-			s.buf = s.buf[n:]
-			return n, nil
-		}
-		select {
-		case <-s.readDeadline.Wait():
-			return 0, os.ErrDeadlineExceeded
-		case <-s.closeCh:
-			return 0, s.closeErr
-		default:
-		}
-		s.bufMu.Unlock()
-		select {
-		case <-s.wakeCh:
-		case <-s.readDeadline.Wait():
-			s.bufMu.Lock()
-			return 0, os.ErrDeadlineExceeded
-		case <-s.closeCh:
-			s.bufMu.Lock()
-			return 0, s.closeErr
-		}
-		s.bufMu.Lock()
-	}
+s.readCond = sync.NewCond(&s.readMu)
+go s.watchReadDeadline()
+if sess.isClient && sess.peerVersion.Load() >= 2 && id >= 1 {
+s.setHandshakeTimer(sess.config.HandshakeTimeout)
 }
-
+return s
+}
+func (s *Stream) watchReadDeadline() {
+for {
+select {
+case <-s.closeCh:
+return
+case <-s.readDeadline.Wait():
+s.readMu.Lock()
+s.readCond.Broadcast()
+s.readMu.Unlock()
+}
+}
+}
+func (s *Stream) setHandshakeTimer(d time.Duration) {
+s.timerMu.Lock()
+defer s.timerMu.Unlock()
+if atomic.LoadUint32(&s.handshakeDone) == 1 {
+return
+}
+if s.handshakeTimer != nil {
+s.handshakeTimer.Stop()
+}
+s.handshakeTimer = time.AfterFunc(d, func() {
+s.sess.Close()
+})
+}
+func (s *Stream) stopHandshakeTimer() {
+s.timerMu.Lock()
+if s.handshakeTimer != nil {
+s.handshakeTimer.Stop()
+s.handshakeTimer = nil
+}
+atomic.StoreUint32(&s.handshakeDone, 1)
+s.timerMu.Unlock()
+s.handshakeChanOnce.Do(func() {
+close(s.handshakeCh)
+})
+}
+func (s *Stream) HandshakeDone() <-chan struct{} {
+return s.handshakeCh
+}
+func (s *Stream) HandshakeError() error {
+s.handshakeErrMu.RLock()
+defer s.handshakeErrMu.RUnlock()
+return s.handshakeErr
+}
+func (s *Stream) WaitHandshake() error {
+<-s.handshakeCh
+return s.HandshakeError()
+}
+func (s *Stream) setHandshakeError(err error) {
+s.handshakeErrMu.Lock()
+s.handshakeErr = err
+s.handshakeErrMu.Unlock()
+}
+func (s *Stream) Read(b []byte) (int, error) {
+if len(b) == 0 {
+return 0, nil
+}
+s.readMu.Lock()
+defer s.readMu.Unlock()
+for {
+if len(s.readBuf) > 0 {
+n := copy(b, s.readBuf)
+s.readBuf = s.readBuf[n:]
+if len(s.readBuf) == 0 {
+s.readBuf = nil
+}
+return n, nil
+}
+if s.readDeadline.Expired() {
+return 0, os.ErrDeadlineExceeded
+}
+select {
+case <-s.closeCh:
+return 0, s.closeErr
+default:
+}
+s.readCond.Wait()
+}
+}
 func (s *Stream) Write(b []byte) (int, error) {
-	select {
-	case <-s.writeDeadline.Wait():
-		return 0, os.ErrDeadlineExceeded
-	case <-s.closeCh:
-		return 0, s.closeErr
-	default:
-	}
-	if s.sess == nil {
-		return 0, io.ErrClosedPipe
-	}
-	return s.sess.writeDataFrame(s.id, b)
+if len(b) == 0 {
+return 0, nil
 }
-
+s.writeMu.Lock()
+defer s.writeMu.Unlock()
+if s.writeDeadline.Expired() {
+return 0, os.ErrDeadlineExceeded
+}
+select {
+case <-s.closeCh:
+return 0, s.closeErr
+default:
+}
+remaining := len(b)
+written := 0
+for remaining > 0 {
+if s.writeDeadline.Expired() {
+return written, os.ErrDeadlineExceeded
+}
+select {
+case <-s.closeCh:
+return written, s.closeErr
+default:
+}
+chunkSize := remaining
+if chunkSize > s.sess.config.MaxFrameSize {
+chunkSize = s.sess.config.MaxFrameSize
+}
+n, err := s.sess.writeDataFrame(s.id, b[written:written+chunkSize], s.writeDeadline)
+if err != nil {
+s.closeLocked(err, false)
+s.sess.Close()
+return written + n, err
+}
+if n < chunkSize {
+return written + n, io.ErrShortWrite
+}
+written += n
+remaining -= n
+}
+return written, nil
+}
 func (s *Stream) Close() error {
-	return s.closeWithError(io.ErrClosedPipe, true)
+s.closeLocked(io.ErrClosedPipe, true)
+return nil
 }
-
-func (s *Stream) closeLocally() {
-	s.closeOnce.Do(func() {
-		s.closeErr = net.ErrClosed
-		close(s.closeCh)
-	})
+func (s *Stream) closeLocked(err error, sendFIN bool) {
+s.closeOnce.Do(func() {
+s.closeErr = err
+close(s.closeCh)
+s.readMu.Lock()
+s.readCond.Broadcast()
+s.readMu.Unlock()
+s.handshakeErrMu.Lock()
+if s.handshakeErr == nil {
+select {
+case <-s.handshakeCh:
+default:
+s.handshakeErr = ErrStreamClosed
 }
-
-func (s *Stream) closeWithError(err error, sendFIN bool) error {
-	s.closeOnce.Do(func() {
-		s.closeErr = err
-		close(s.closeCh)
-	})
-	if sendFIN && s.closeErr != nil {
-		_ = s.sess.streamClosed(s.id)
-	}
-	return s.closeErr
 }
-
+s.handshakeErrMu.Unlock()
+s.stopHandshakeTimer()
+if sendFIN && !s.sess.IsClosed() {
+go s.sess.writeFrame(newFrame(cmdFIN, s.id, nil))
+s.sess.streamClosed(s.id)
+}
+})
+}
 func (s *Stream) pushData(data []byte) bool {
-	s.bufMu.Lock()
-	if len(s.buf)+len(data) > s.maxBufSize {
-		s.bufMu.Unlock()
-		return false
-	}
-	s.buf = append(s.buf, data...)
-	s.bufMu.Unlock()
-	select {
-	case s.wakeCh <- struct{}{}:
-	default:
-	}
-	return true
+select {
+case <-s.closeCh:
+return false
+default:
 }
-
+s.readMu.Lock()
+if len(s.readBuf)+len(data) > s.maxBufSize {
+s.readMu.Unlock()
+return false
+}
+s.readBuf = append(s.readBuf, data...)
+s.readMu.Unlock()
+s.readCond.Broadcast()
+return true
+}
 func (s *Stream) SetReadDeadline(t time.Time) error {
-	s.readDeadline.Set(t)
-	select {
-	case s.wakeCh <- struct{}{}:
-	default:
-	}
-	return nil
+s.readDeadline.Set(t)
+s.readMu.Lock()
+s.readCond.Broadcast()
+s.readMu.Unlock()
+return nil
 }
-
-func (s *Stream) SetWriteDeadline(t time.Time) error { s.writeDeadline.Set(t); return nil }
-func (s *Stream) SetDeadline(t time.Time) error       { s.SetReadDeadline(t); s.SetWriteDeadline(t); return nil }
-
+func (s *Stream) SetWriteDeadline(t time.Time) error {
+s.writeDeadline.Set(t)
+return nil
+}
+func (s *Stream) SetDeadline(t time.Time) error {
+s.SetReadDeadline(t)
+s.SetWriteDeadline(t)
+return nil
+}
 func (s *Stream) LocalAddr() net.Addr {
-	if c, ok := s.sess.conn.(interface{ LocalAddr() net.Addr }); ok {
-		return c.LocalAddr()
-	}
-	return nil
+return s.sess.conn.LocalAddr()
 }
-
 func (s *Stream) RemoteAddr() net.Addr {
-	if c, ok := s.sess.conn.(interface{ RemoteAddr() net.Addr }); ok {
-		return c.RemoteAddr()
-	}
-	return nil
+return s.sess.conn.RemoteAddr()
 }
-
 func (s *Stream) HandshakeSuccess() error {
-	var once bool
-	s.handshakeOnce.Do(func() { once = true })
-	if once && s.sess.peerVersion >= 2 {
-		_, err := s.sess.writeControlFrame(newFrame(cmdSYNACK, s.id))
-		return err
-	}
-	return nil
+if s.sess.peerVersion.Load() < 2 {
+return errors.New("mux: handshake not supported by peer")
 }
-
+var done bool
+s.handshakeOnce.Do(func() { done = true })
+if done {
+s.stopHandshakeTimer()
+return s.sess.writeFrame(newFrame(cmdSYNACK, s.id, nil))
+}
+return nil
+}
 func (s *Stream) HandshakeFailure(err error) error {
-	var once bool
-	s.handshakeOnce.Do(func() { once = true })
-	if once && err != nil && s.sess.peerVersion >= 2 {
-		f := newFrame(cmdSYNACK, s.id)
-		f.data = []byte(err.Error())
-		_, err := s.sess.writeControlFrame(f)
-		return err
-	}
-	return nil
+if s.sess.peerVersion.Load() < 2 {
+return errors.New("mux: handshake not supported by peer")
 }
-
-type PipeDeadline struct {
-	mu     sync.Mutex
-	timer  *time.Timer
-	cancel chan struct{}
+var done bool
+s.handshakeOnce.Do(func() { done = true })
+if done && err != nil {
+s.setHandshakeError(err)
+s.stopHandshakeTimer()
+return s.sess.writeFrame(newFrame(cmdSYNACK, s.id, []byte(err.Error())))
 }
-
-func MakePipeDeadline() PipeDeadline {
-	return PipeDeadline{cancel: make(chan struct{})}
-}
-
-func (d *PipeDeadline) Set(t time.Time) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	if d.timer != nil && !d.timer.Stop() {
-		<-d.cancel
-	}
-	d.timer = nil
-	closed := isClosedChan(d.cancel)
-	if t.IsZero() {
-		if closed {
-			d.cancel = make(chan struct{})
-		}
-		return
-	}
-	if dur := time.Until(t); dur > 0 {
-		if closed {
-			d.cancel = make(chan struct{})
-		}
-		d.timer = time.AfterFunc(dur, func() {
-			close(d.cancel)
-		})
-		return
-	}
-	if !closed {
-		close(d.cancel)
-	}
-}
-
-func (d *PipeDeadline) Wait() chan struct{} {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	return d.cancel
-}
-
-func isClosedChan(c <-chan struct{}) bool {
-	select {
-	case <-c:
-		return true
-	default:
-		return false
-	}
+return nil
 }
